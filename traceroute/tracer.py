08611f179869aabab8e8e8e6a79cd22a529071d8
mark <markenavuk@bk.ru>
2021-05-20 11:59:10 +0500

    Added regexp

--- traceroute/tracer.py
+++ traceroute/tracer.py
@@ -1,9 +1,12 @@
+import re
 import socket
-import struct
 from typing import Dict, Iterable, Optional
 
 from traceroute.icmp import IcmpPacket
 from traceroute.traceresult import TraceResult
+
+WHOIS_SERVER_NAME = "whois.iana.org"
+WHOIS_RE = re.compile(r"([A-Za-z\-]+):\s+([^\#\n]+)")
 
 
 class Tracer:
@@ -15,57 +18,37 @@ class Tracer:
         if self.destination == socket.gethostbyname('localhost'):
             self.depth = 1
 
-    @staticmethod
-    def get_whois_iana_data(addr) -> str:
+    @classmethod
+    def get_whois_iana_data(cls, addr: str) -> str:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as whois_sock:
             whois_sock.settimeout(1)
-            whois_sock.connect((socket.gethostbyname('whois.iana.org'), 43))
+            whois_sock.connect((socket.gethostbyname(WHOIS_SERVER_NAME), 43))
             whois_sock.send(addr.encode(encoding='utf-8') + b'\r\n')
             try:
-                iana_response = whois_sock.recv(1024).decode()
-                whois_addr_start = iana_response.index('whois')
-                whois_addr_end = iana_response.index('\n', whois_addr_start)
-                whois_addr = iana_response[whois_addr_start:whois_addr_end].\
-                    replace(' ', '').split(':')[0]
-                return whois_addr
+                data = cls.receive_data(whois_sock)
+                return cls.parse_whois_response(data.decode()).get("whois", "")
             except (socket.timeout, ValueError):
                 return ""
 
     def get_whois_data(self, addr: str) -> Optional[Dict]:
         whois_addr = self.get_whois_iana_data(addr)
-        whois_data = {}
+
         if not whois_addr:
             return
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as whois_sock:
             whois_sock.settimeout(2)
             whois_sock.connect((whois_addr, 43))
             whois_sock.send(addr.encode(encoding='utf-8') + b'\r\n')
-            data = b''
-            while True:
-                temp_data = whois_sock.recv(1024)
-                if not temp_data:
-                    break
-                data += temp_data
-            data = data.decode()
-            for field in ('netname', 'country', 'origin'):
-                try:
-                    field_start = data.rindex(f'{field}:')
-                    field_end = data.index('\n', field_start)
-                    field_data = data[field_start:field_end].\
-                        replace(' ', '').split(':')[1]
-                    whois_data[field] = field_data
-                except ValueError:
-                    continue
+            data = self.receive_data(whois_sock)
+            whois_data = self.parse_whois_response(data.decode())
         whois_data['route'] = addr
         return whois_data
 
     def start(self) -> Iterable[TraceResult]:
-        n = 1
         while self.time_to_live <= self.depth:
-            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
-                               socket.IPPROTO_ICMP) as sender, \
-                    socket.socket(socket.AF_INET, socket.SOCK_RAW,
-                                  socket.IPPROTO_ICMP) as receiver:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_ICMP) as sender, \
+                    socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as receiver:
                 sender.setsockopt(socket.SOL_IP, socket.IP_TTL, self.time_to_live)
                 receiver.settimeout(3)
                 sender.sendto(IcmpPacket(8, 0).pack(), (self.destination, 80))
@@ -73,8 +56,8 @@ class Tracer:
                     data, addr = receiver.recvfrom(1024)
                     whois_data = self.get_whois_data(addr[0])
                     icmp_response = IcmpPacket.from_bytes(data[20:])
-                    trace_result = TraceResult.get_from_data(addr[0], n, whois_data)
-                    n += 1
+                    trace_result = TraceResult.get_from_data(addr[0],
+                                                             whois_data)
                     yield trace_result
                     if icmp_response.code == icmp_response.type == 0:
                         break
@@ -82,3 +65,18 @@ class Tracer:
                     continue
                 finally:
                     self.time_to_live += 1
+
+    @staticmethod
+    def receive_data(sock: socket.socket) -> bytes:
+        data = b""
+        while True:
+            temp_data = sock.recv(1024)
+            if not temp_data:
+                break
+            data += temp_data
+        return data
+
+    @staticmethod
+    def parse_whois_response(data: str) -> Dict:
+        result = re.findall(WHOIS_RE, data)
+        return {key: value for key, value in result}
--- traceroute/traceresult.py
+++ traceroute/traceresult.py
@@ -5,23 +5,22 @@ from typing import Dict
 @dataclass
 class TraceResult:
     route: str
-    n: int
     net_name: str
     as_zone: str
     country: str
     is_local: bool
 
     @classmethod
-    def get_from_data(cls, addr: str, n: int, data: Dict):
+    def get_from_data(cls, addr: str, data: Dict):
         is_local = data is None
         route = data.get('route', '') if not is_local else addr
         net_name = data.get('netname', '') if not is_local else ''
         as_zone = data.get('origin', '') if not is_local else ''
         country = data.get('country', '') if not is_local else ''
-        return cls(route, n, net_name, as_zone, country, is_local)
+        return cls(route, net_name, as_zone, country, is_local)
 
     def __str__(self) -> str:
-        result = f'{self.n}. {self.route}\n'
+        result = f'{self.route}\n'
         if self.is_local:
             return f'{result}local'
         info = []
@@ -32,4 +31,4 @@ class TraceResult:
         if self.country and self.country != 'EU':
             info.append(self.country)
         return result + ', '.join(info) if len(info) > 0 \
-            else f'{self.n}. *\r'
+            else '*\t'
